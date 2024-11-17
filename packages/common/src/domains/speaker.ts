d0306d8286bbc378049fae42cb2f6d385d5da834
Maxime Flamant <maxime.flamant@gmail.com>
2024-11-17 15:43:31 +0100

    MediaPlayerDeviceClass + update SpeakerDeviceAttributes

--- packages/common/src/domains/speaker.ts
+++ packages/common/src/domains/speaker.ts
@@ -1,3 +1,11 @@
+// https://www.home-assistant.io/integrations/media_player/
+export enum MediaPlayerDeviceClass {
+  Tv = "tv",
+  Speaker = "speaker",
+  Receiver = "receiver",
+}
+
 export interface SpeakerDeviceAttributes {
   volume_level?: number;
+  device_class?: MediaPlayerDeviceClass;
 }
