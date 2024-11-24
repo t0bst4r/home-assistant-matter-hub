189fcbbe20a99ef6f21d1efc6c3fb0f272b5cbf8
adri4555 <adri_4555@outlook.es>
2024-11-24 23:16:54 +0100

    feat: Change row fields spacing

--- packages/frontend/src/components/fields-editor/index.tsx
+++ packages/frontend/src/components/fields-editor/index.tsx
@@ -96,7 +96,7 @@ const EditionFilter = ({
         }}
       >
         <CardContent>
-          <Grid2 container rowSpacing={1.5} >
+          <Grid2 container rowSpacing={2} >
             <Grid2 size={12}   >
               <Typography gutterBottom variant="h6" component="div">
                 {title}
@@ -141,44 +141,47 @@ const Filter = ({ value, onChange, onDelete }: FilterProps) => {
     });
   };
   return (
-    <Grid2 container spacing={{md: 1, xs:1.5}} size={12}>
-      <Grid2 size={{xs: 12, sm: 3, md: 2}}>
-        <FormControl fullWidth>
-          <InputLabel id="select-field-type">Type</InputLabel>
-          <Select
-            onChange={(event) =>
-              handleOnChange(event.target.value as any, "type")
-            }
-            labelId="select-field-type"
-            id="demo-simple-select"
-            value={value.type}
-            label="Age"
-          >
-            <MenuItem value={"pattern"}>Pattern</MenuItem>
-            <MenuItem value={"domain"}>Domain</MenuItem>
-            <MenuItem value={"platform"}>Platform</MenuItem>
-            <MenuItem value={"label"}>Label</MenuItem>
-          </Select>
-        </FormControl>
-      </Grid2>
-      <Grid2 size={{xs: 12, sm: 7, md: 9}}>
-        <TextField
-          onChange={(event) => handleOnChange(event.target.value as any, "value")}
-          label="Value"
-          variant="outlined"
-          value={value.value}
-          fullWidth
-        />
-      </Grid2>
-      <Grid2 size={{xs: 12, sm: 2, md: 1}} >
-        <Button style={{height: "100%", width:"100%", minHeight: 0}} size={"small"} variant="outlined" color="error" onClick={onDelete}  >
-          <DeleteOutlineIcon />
-        </Button>
+    <>
+      <Grid2 container spacing={{md: 1, xs:1.5}} size={12}>
+        <Grid2 size={{xs: 12, sm: 3, md: 2}}>
+          <FormControl fullWidth>
+            <InputLabel id="select-field-type">Type</InputLabel>
+            <Select
+              onChange={(event) =>
+                handleOnChange(event.target.value as any, "type")
+              }
+              labelId="select-field-type"
+              id="demo-simple-select"
+              value={value.type}
+              label="Age"
+            >
+              <MenuItem value={"pattern"}>Pattern</MenuItem>
+              <MenuItem value={"domain"}>Domain</MenuItem>
+              <MenuItem value={"platform"}>Platform</MenuItem>
+              <MenuItem value={"label"}>Label</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid2>
+        <Grid2 size={{xs: 12, sm: 7, md: 9}}>
+          <TextField
+            onChange={(event) => handleOnChange(event.target.value as any, "value")}
+            label="Value"
+            variant="outlined"
+            value={value.value}
+            fullWidth
+          />
+        </Grid2>
+        <Grid2 size={{xs: 12, sm: 2, md: 1}} >
+          <Button style={{height: "100%", width:"100%", minHeight: 0}} size={"small"} variant="outlined" color="error" onClick={onDelete}  >
+            <DeleteOutlineIcon />
+          </Button>
+        </Grid2>
+        
       </Grid2>
       <Grid2 size={12}>
         <Divider  />
       </Grid2>
-    </Grid2>
+    </>
   );
 };
 
