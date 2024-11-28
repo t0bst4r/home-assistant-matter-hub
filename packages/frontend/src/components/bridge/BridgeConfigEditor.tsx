dcfec18a2f136f103c10148d3d62051443ef59e6
Tobias Glatthar <t0bst4r@outlook.com>
2024-11-28 12:11:01 +0100

    refactor(frontend): build a rich form editor based on json schema

--- packages/frontend/src/components/bridge/BridgeConfigEditor.tsx
+++ packages/frontend/src/components/bridge/BridgeConfigEditor.tsx
@@ -1,99 +1,138 @@
+import { Box, Button, Stack } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { useCallback, useState } from "react";
 import {
   BridgeConfig,
   bridgeConfigSchema,
 } from "@home-assistant-matter-hub/common";
-import type { JSONSchema7 } from "json-schema";
-import { JsonEditor } from "../misc/JsonEditor.tsx";
-import { Box, Button, Typography } from "@mui/material";
-import { FieldsEditor } from "../fields-editor/FieldsEditor.tsx";
-import { useState } from "react";
+import { JsonEditor } from "../misc/editors/JsonEditor";
+import { FormEditor } from "../misc/editors/FormEditor";
 import { LibraryBooks, TextFields } from "@mui/icons-material";
-
-const ignoredProperties = ["name", "port"] as const;
-type IgnoredProperties = (typeof ignoredProperties)[number];
-
-export type EditableBridgeConfig = Omit<
-  BridgeConfig,
-  (typeof ignoredProperties)[number]
->;
-const editableConfigSchema: JSONSchema7 = {
-  ...bridgeConfigSchema,
-  properties: {
-    ...bridgeConfigSchema.properties,
-  },
-  required: bridgeConfigSchema.required?.filter(
-    (it) => !ignoredProperties.includes(it as IgnoredProperties),
-  ),
-};
-ignoredProperties.forEach((prop) => {
-  if (editableConfigSchema.properties) {
-    delete editableConfigSchema.properties[prop];
-  }
-});
-
-export interface BridgeConfigEditorProps {
-  config: EditableBridgeConfig;
-  isValid: boolean;
-  onChange: (
-    config: EditableBridgeConfig | undefined,
-    isValid: boolean,
-  ) => void;
-}
+import { ValidationError } from "../misc/editors/validation-error.ts";
 
 enum BridgeEditorMode {
   JSON_EDITOR = "JSON_EDITOR",
   FIELDS_EDITOR = "FIELDS_EDITOR",
 }
 
-export const BridgeConfigEditor = ({
-  config,
-  onChange,
-  isValid,
-}: BridgeConfigEditorProps) => {
-  const [editionMode, setEditionMode] = useState(
+export interface BridgeConfigEditorProps {
+  bridgeId?: string;
+  bridge: BridgeConfig;
+  usedPorts: Record<number, string>;
+  onSave: (config: BridgeConfig) => void | Promise<void>;
+  onCancel: () => void | Promise<void>;
+}
+
+export const BridgeConfigEditor = (props: BridgeConfigEditorProps) => {
+  const [editorMode, setEditorMode] = useState<BridgeEditorMode>(
     BridgeEditorMode.FIELDS_EDITOR,
   );
-  const valueChanged = (event: {
-    isValid: boolean;
-    value?: EditableBridgeConfig;
-  }) => {
-    onChange(event.value, event.isValid);
-  };
-
-  const toggleEditionType = () => {
-    setEditionMode(
-      editionMode === BridgeEditorMode.FIELDS_EDITOR
+  const toggleEditor = () => {
+    setEditorMode(
+      editorMode === BridgeEditorMode.FIELDS_EDITOR
         ? BridgeEditorMode.JSON_EDITOR
         : BridgeEditorMode.FIELDS_EDITOR,
     );
   };
 
+  const [config, setConfig] = useState<object | undefined>(props.bridge);
+  const [isValid, setIsValid] = useState<boolean>(true);
+
+  const validatePort = useCallback(
+    (value: object | undefined): ValidationError[] => {
+      const config = value as Partial<BridgeConfig> | undefined;
+      if (!config?.port) {
+        return [];
+      }
+      const usedBy = props.usedPorts[config.port];
+      if (usedBy != undefined && usedBy != props.bridgeId) {
+        return [
+          {
+            instancePath: "/port",
+            message: `Port is already used by bridge with id ${usedBy}`,
+          },
+        ];
+      }
+      return [];
+    },
+    [props.bridgeId, props.usedPorts],
+  );
+
+  const onChange = (data: object | undefined, isValid: boolean) => {
+    setConfig(data);
+    setIsValid(isValid);
+  };
+
+  const saveAction = async () => {
+    if (!isValid) {
+      return;
+    }
+    await props.onSave(config as BridgeConfig);
+  };
+
   return (
-    <Box>
-      <Box display="flex" justifyContent={"space-between"}>
-        <Typography gutterBottom variant="h5" component="div">
-          {editionMode === BridgeEditorMode.FIELDS_EDITOR
-            ? "Fields Editor"
-            : "Json Editor"}
-        </Typography>
-        <Button onClick={() => toggleEditionType()} disabled={!isValid}>
-          {editionMode === BridgeEditorMode.FIELDS_EDITOR ? (
+    <Stack spacing={2}>
+      <Box display="flex" justifyContent={"flex-end"}>
+        <Button
+          onClick={() => toggleEditor()}
+          title={
+            editorMode === BridgeEditorMode.FIELDS_EDITOR
+              ? "JSON editor"
+              : "Form editor"
+          }
+        >
+          {editorMode === BridgeEditorMode.FIELDS_EDITOR ? (
             <TextFields />
           ) : (
             <LibraryBooks />
           )}
         </Button>
       </Box>
-      {editionMode === BridgeEditorMode.FIELDS_EDITOR && (
-        <FieldsEditor value={config} onChange={valueChanged} />
-      )}
-      {editionMode === BridgeEditorMode.JSON_EDITOR && (
-        <JsonEditor
-          value={config}
-          onChange={valueChanged}
-          schema={editableConfigSchema}
+
+      {editorMode === BridgeEditorMode.FIELDS_EDITOR && (
+        <FormEditor
+          value={config ?? {}}
+          onChange={onChange}
+          schema={bridgeConfigSchema}
+          customValidate={validatePort}
         />
       )}
-    </Box>
+
+      {editorMode === BridgeEditorMode.JSON_EDITOR && (
+        <JsonEditor
+          value={config ?? {}}
+          onChange={onChange}
+          schema={bridgeConfigSchema}
+          customValidate={validatePort}
+        />
+      )}
+
+      <Grid container>
+        <Grid size={{ xs: 6, sm: 4, md: 3 }}>
+          <Button
+            fullWidth
+            variant="outlined"
+            color="error"
+            onClick={props.onCancel}
+          >
+            Cancel
+          </Button>
+        </Grid>
+        <Grid
+          size={{ xs: 0, sm: 4, md: 6 }}
+          sx={{ display: { xs: "none", sm: "block" } }}
+        />
+        <Grid size={{ xs: 6, sm: 4, md: 3 }}>
+          <Button
+            fullWidth
+            variant="outlined"
+            disabled={!isValid}
+            onClick={saveAction}
+          >
+            Save
+          </Button>
+        </Grid>
+      </Grid>
+    </Stack>
   );
 };
