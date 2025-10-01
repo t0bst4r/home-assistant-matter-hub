df983ed9119ce7c7edbcbbc7881ff1552cbda704
Jeffrey Larson <jeffothy@gmail.com>
2025-10-01 14:42:39 -0700

    fix: log but avoid crash for invalid device errors

--- packages/backend/src/services/bridges/bridge-endpoint-manager.ts
+++ packages/backend/src/services/bridges/bridge-endpoint-manager.ts
@@ -1,8 +1,10 @@
+import type { Logger } from "@matter/general";
 import type { Endpoint } from "@matter/main";
 import { Service } from "../../core/ioc/service.js";
 import { AggregatorEndpoint } from "../../matter/endpoints/aggregator-endpoint.js";
 import type { EntityEndpoint } from "../../matter/endpoints/entity-endpoint.js";
 import { LegacyEndpoint } from "../../matter/endpoints/legacy/legacy-endpoint.js";
+import { InvalidDeviceError } from "../../utils/errors/invalid-device-error.js";
 import { subscribeEntities } from "../home-assistant/api/subscribe-entities.js";
 import type { HomeAssistantClient } from "../home-assistant/home-assistant-client.js";
 import type { HomeAssistantStates } from "../home-assistant/home-assistant-registry.js";
@@ -16,6 +18,7 @@ export class BridgeEndpointManager extends Service {
   constructor(
     private readonly client: HomeAssistantClient,
     private readonly registry: BridgeRegistry,
+    private readonly log: Logger,
   ) {
     super("BridgeEndpointManager");
     this.root = new AggregatorEndpoint("aggregator");
@@ -62,7 +65,22 @@ export class BridgeEndpointManager extends Service {
     for (const entityId of this.entityIds) {
       let endpoint = existingEndpoints.find((e) => e.entityId === entityId);
       if (!endpoint) {
-        endpoint = await LegacyEndpoint.create(this.registry, entityId);
+        try {
+          endpoint = await LegacyEndpoint.create(this.registry, entityId);
+        } catch (e) {
+          if (e instanceof InvalidDeviceError) {
+            this.log.warn(
+              `Invalid device detected. Entity: ${entityId} Reason: ${(e as Error).message}`,
+            );
+            continue;
+          } else {
+            this.log.error(
+              `Failed to create device ${entityId}. Error: ${e?.toString()}`,
+            );
+            throw e;
+          }
+        }
+
         if (endpoint) {
           await this.root.add(endpoint);
         }
