51f84e10c11f9031b1e919a46be6cae1005b0320
t0bst4r <t0bst4r@outlook.com>
2025-10-01 10:51:18 +0200

    fix: finally fix bigint serialization to be compatible with matter.js

--- packages/backend/src/polyfills.ts
+++ packages/backend/src/polyfills.ts
@@ -4,6 +4,10 @@ declare global {
   }
 }
 
+// Copied from: toJson in @matter/main
+
+const JSON_SPECIAL_KEY_TYPE = "__object__";
+const JSON_SPECIAL_KEY_VALUE = "__value__";
 BigInt.prototype.toJSON = function () {
-  return this.toString();
+  return `{"${JSON_SPECIAL_KEY_TYPE}":"BigInt","${JSON_SPECIAL_KEY_VALUE}":"${this.toString()}"}`;
 };
