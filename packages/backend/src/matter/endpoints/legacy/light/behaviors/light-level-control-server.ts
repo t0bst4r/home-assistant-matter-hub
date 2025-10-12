eb913b28de17cf8db80b2c877adcd77ef4538871
chah90 <chah90@users.noreply.github.com>
2025-10-12 11:35:50 +0000

    executeIfOff is now always active for color-control and level-control -> not affected by new Alexa feature flag anymore

--- packages/backend/src/matter/endpoints/legacy/light/behaviors/light-level-control-server.ts
+++ packages/backend/src/matter/endpoints/legacy/light/behaviors/light-level-control-server.ts
@@ -6,8 +6,6 @@ import {
   type LevelControlConfig,
   LevelControlServer,
 } from "../../../../behaviors/level-control-server.js";
-import type { Agent } from "@matter/main";
-import { BridgeDataProvider } from "../../../../../services/bridges/bridge-data-provider.js";
 
 const config: LevelControlConfig = {
   getValuePercent: (state: HomeAssistantEntityState<LightDeviceAttributes>) => {
@@ -23,10 +21,6 @@ const config: LevelControlConfig = {
       brightness: Math.round(brightnessPercent * 255),
     },
   }),
-  executeIfOff: (_entity: HomeAssistantEntityState, agent: Agent) => {
-    const { featureFlags } = agent.env.get(BridgeDataProvider);
-    return featureFlags?.alexaLightOptimizations === true;
-  },
 };
 
 export const LightLevelControlServer = LevelControlServer(config).with([
